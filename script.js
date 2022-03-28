daf58d88b2db43234d60ef5fb902713759473614
MikhailGutsaluyk <mikhail0731@gmail.com>
2022-03-27 21:43:23 -0600

    stuck with button

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const newTask = document.querySelector
+('#new-task')
+const addTaskBtn = document.querySelector
+('#add-btn')
+const taskList = document.querySelector
+('#task-list')
+console.log(taskList)
+const handleAddTask =() => {
+  console.log(newTask.value)  
+}
+
+
+const newTaskLi =document.createElement('li')
+const newTaskSpan =document.createElement('span')
+const newTaskBtn =document.createElement('button')
+
+taskList.appendChild(newTaskLi)
+newTaskLi.appendChild(newTaskSpan)
+newTaskSpan.appendChild(newTaskBtn)
+
+newTaskLi.classList.add('task-container', 'list-group-item')
+newTaskBtn.classList.add('btn', 'btn-danger')
+
+
+
+
+newTaskBtn.textContent ='x'
+//newTaskSpan.textContent = 'Dummy'
+
+
+
+
+
+
+
